1c392983ddf4faa032c881d547fe12f2dd015e2e
root <root@inf01.alytics.org>
2014-09-26 23:46:54 +0400

    plugins

---
+++ plugins/redmine_spent_time_required/init.rb
@@ -0,0 +1,30 @@
+require 'redmine'
+require File.dirname(__FILE__) + '/lib/issues_controller_patch.rb'
+
+if Rails::VERSION::MAJOR >= 3
+  ActionDispatch::Callbacks.to_prepare do
+    require_dependency 'issues_controller'
+    IssuesController.send(:include, RedmineSpentTimeRequired::Patches::IssuesControllerPatch)
+  end
+else
+  require 'dispatcher'
+  Dispatcher.to_prepare do
+    require_dependency 'issues_controller'
+    IssuesController.send(:include, RedmineSpentTimeRequired::Patches::IssuesControllerPatch)
+  end
+end
+
+Redmine::Plugin.register :redmine_spent_time_required do
+  name 'Redmine Spent Time Required'
+  author 'Max Prokopiev'
+  description 'Plugin to require adding spent time'
+  version '0.0.1'
+  url 'http://trs.io/'
+  author_url 'http://github.com/juggler'
+
+  settings(:default => {
+             'statuses' => '3,5',
+             'projects' => ''
+           }, :partial => 'settings/spent_time_settings')
+
+end
